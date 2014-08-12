8c614b1274defb2ba6569f7e413a4a2698ae0abf
FCPDIT <ditfcp2014@gmail.com>
2014-08-12 10:25:23 +0100

    Update Invoice.java

--- RetailSystem/src/retail/Invoice.java
+++ RetailSystem/src/retail/Invoice.java
@@ -1,9 +1,86 @@
 package retail;
 
-public class Invoice {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-	public Invoice() {
-		// TODO Auto-generated constructor stub
+public class Invoice {
+	private Employee employee; // or maybe just an employeeId ??
+	private Customer customer;
+	private Product product;
+	private int quantity;
+	private double invoiceTotal; // ?? quantity*retailPrice of product
+	private Date invoiceDate;
+	
+
+	public Invoice(Employee employee, Customer customer, Product product, int quantity) {
+		this.product = product;
+		this.customer = customer;
+		this.employee = employee;
+		this.quantity = quantity;
+		invoiceTotal = product.getRetailPrice() * this.quantity;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy");
+		invoiceDate = new Date();
+	}
+
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+
+	public Product getProduct() {
+		return product;
+	}
+
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public double getInvoiceTotal() {
+		return invoiceTotal;
+	}
+
+
+	public void setInvoiceTotal(double invoiceTotal) {
+		this.invoiceTotal = invoiceTotal;
+	}
+
+
+	public Date getInvoiceDate() {
+		return invoiceDate;
+	}
+
+
+	public void setInvoiceDate(Date invoiceDate) {
+		this.invoiceDate = invoiceDate;
 	}
 
 }
+
