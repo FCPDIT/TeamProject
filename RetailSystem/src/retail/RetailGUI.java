b6e9919c9815031c2e7c4c2271ec666afd0f02ca
Andrew <ditfcp2014@gmail.com>
2014-08-12 18:55:13 +0100

    Andrew: Added Basic Frame for GUI

---
+++ RetailSystem/src/retail/RetailGUI.java
@@ -0,0 +1,62 @@
+package retail;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class RetailGUI extends JFrame{
+	
+	private JFrame mainJFrame = new JFrame();
+	private JTabbedPane mainJTabbedPane = new JTabbedPane();
+	
+	private JPanel loginJPanel = new JPanel();
+	private JTabbedPane custJTabbedPane = new JTabbedPane();
+	private JTabbedPane supplyJTabbedPane = new JTabbedPane();
+	private JTabbedPane prodJTabbedPane = new JTabbedPane();
+	private JTabbedPane invJTabbedPane = new JTabbedPane();
+	private JTabbedPane stockJTabbedPane = new JTabbedPane();
+	private JTabbedPane accessJTabbedPane = new JTabbedPane();
+	private JTabbedPane proflossJTabbedPane = new JTabbedPane();
+	
+	private JPanel addCustJPanel = new JPanel();
+	private JPanel viewCustJPanel = new JPanel();
+	private JPanel editCustJPanel = new JPanel();
+	
+	private JPanel addSupplyJPanel = new JPanel();
+	private JPanel viewSupplyJPanel = new JPanel();
+	private JPanel editSupplyJPanel = new JPanel();
+	
+	private JPanel addProductJPanel = new JPanel();
+	private JPanel viewProductJPanel = new JPanel();
+	private JPanel editProductJPanel = new JPanel();
+	
+	public RetailGUI() {
+		// TODO Auto-generated constructor stub
+		mainJFrame.setTitle("Retail Application");
+		mainJFrame.setBounds(0, 0, 800, 600);
+		mainJFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mainJFrame.setVisible(true);
+		mainJFrame.add(mainJTabbedPane);
+		
+		mainJTabbedPane.add("Login", loginJPanel);
+		mainJTabbedPane.add("Customer Management", custJTabbedPane);
+		mainJTabbedPane.add("Supplier Management", supplyJTabbedPane);
+		mainJTabbedPane.add("Product Management", prodJTabbedPane);
+		mainJTabbedPane.add("Invoice Management", invJTabbedPane);
+		mainJTabbedPane.add("Stock Management", stockJTabbedPane);
+		mainJTabbedPane.add("Manage Employee Access", accessJTabbedPane);
+		mainJTabbedPane.add("Profit and Loss", proflossJTabbedPane);
+		
+		custJTabbedPane.add("Create New Customer",addCustJPanel);
+		custJTabbedPane.add("View Customer Details", viewCustJPanel);
+		custJTabbedPane.add("Edit Customer Details", editCustJPanel);
+		
+		supplyJTabbedPane.add("Create New Supplier",addSupplyJPanel);
+		supplyJTabbedPane.add("View Supplier Details", viewSupplyJPanel);
+		supplyJTabbedPane.add("Edit Supplier Details", editSupplyJPanel);
+		
+		prodJTabbedPane.add("Create New Product",addProductJPanel);
+		prodJTabbedPane.add("View Product Details", viewProductJPanel);
+		prodJTabbedPane.add("Edit Product Details", editProductJPanel);
+	}
+
+}
