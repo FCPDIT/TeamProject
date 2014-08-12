37fe815264e6d4218a51decb9196f4fda79162e0
FCPDIT <ditfcp2014@gmail.com>
2014-08-12 14:05:21 +0100

    Marc: Customer class added

--- RetailSystem/src/retail/Customer.java
+++ RetailSystem/src/retail/Customer.java
@@ -4,9 +4,77 @@ package retail;
 
 
 public class Customer {
-
-	public Customer() {
-		// TODO Auto-generated constructor stub
+	
+	//Variables
+	private int custId;
+	private String custName;
+	private String custAddress;
+	private String custEmail;
+	private String custTelephone;
+	
+	//Constructors
+	public Customer(int custId, String custName, String custAddress,
+			String custEmail, String custTelephone) {
+		this.custId = custId;
+		this.custName = custName;
+		this.custAddress = custAddress;
+		this.custEmail = custEmail;
+		this.custTelephone = custTelephone;
 	}
+
+	public Customer(int custId, String custName, String custAddress) {
+		this.custId = custId;
+		this.custName = custName;
+		this.custAddress = custAddress;
+	}
+
+	//Getter and setters
+	public int getCustId() {
+		return custId;
+	}
+
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+
+	public String getCustName() {
+		return custName;
+	}
+
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+
+	public String getCustAddress() {
+		return custAddress;
+	}
+
+	public void setCustAddress(String custAddress) {
+		this.custAddress = custAddress;
+	}
+
+	public String getCustEmail() {
+		return custEmail;
+	}
+
+	public void setCustEmail(String custEmail) {
+		this.custEmail = custEmail;
+	}
+
+	public String getCustTelephone() {
+		return custTelephone;
+	}
+
+	public void setCustTelephone(String custTelephone) {
+		this.custTelephone = custTelephone;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
