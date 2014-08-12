1eba655d293edbd5f4021a819bfd15cf4e118b8f
James <james.mc.ternan@gmail.com>
2014-08-12 17:04:26 +0100

    James: Array list of products added to RetailSystemDriver. Also for loop added to test that Array list has been populated correctly.

--- RetailSystem/src/retail/RetailSystemDriver.java
+++ RetailSystem/src/retail/RetailSystemDriver.java
@@ -1,16 +1,31 @@
 package retail;
 
-public class RetailSystemDriver {
+import java.util.ArrayList;
 
+public class RetailSystemDriver {
+	
+	private ArrayList<Product> products = new ArrayList<Product>();
+	
 	public RetailSystemDriver() {
-		// TODO Auto-generated constructor stub
+		Product p1 = new Product(" Game of Thrones ", " George R.R Martin ", " 9780553386790 ", 9.99, 3.75,100,200,10,new Supplier(12345," Books Express "," 12 Main Street, Cork "," sales@bookexpress.com ", " 01685789 "));
+		Product p2 = new Product(" Not a Drill ", " Lee Child ", " 9780553389521 ", 12.75, 4.95,50,200,10,new Supplier(12569," Book Warehouse "," 12 Whitehall industrial Park, Dublin "," sales@bookwarehouse.ie ", " 01488759 "));
+	
+		products.add(p1);
+		products.add(p2);
+		
+		// Test Code to ensure Array has been populated.
+		for(Product products : products){ 
+			System.out.println(products.getAuthor());
+		}
+		
+		
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new RetailSystemDriver();
 
 	}
 
