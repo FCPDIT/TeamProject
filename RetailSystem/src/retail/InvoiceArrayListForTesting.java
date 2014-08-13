cb232c3aa51bd1ba74767a335befaa4e3d2cb82d
FCPDIT <ditfcp2014@gmail.com>
2014-08-13 15:41:33 +0100

    Update InvoiceArrayListForTesting.java
    Changing Comment

--- RetailSystem/src/retail/InvoiceArrayListForTesting.java
+++ RetailSystem/src/retail/InvoiceArrayListForTesting.java
@@ -33,8 +33,8 @@ public class InvoiceArrayListForTesting {
 	}
 
 	public static void main(String[] args) {
-		//new InvoiceArrayListForTesting();
-		new RetailGUI();
+		new InvoiceArrayListForTesting();
+		//new RetailGUI();
 	}
 
 }
