8f479e8e5cfcd3240db665a3dd9a6c3bac25fa4d
Hannah Mac Allister <hmacallister@outlook.com>
2014-08-20 21:51:41 +0100

    removed edit invoice quantity and product to make compatible with multiple products

--- RetailSystem/src/retail/RetailGUI.java
+++ RetailSystem/src/retail/RetailGUI.java
@@ -886,17 +886,14 @@ public class RetailGUI extends JFrame{
 		});
 		customerInvoiceJTextArea.setEditable(false);
 		allInvoicesTotalJTextField.setEditable(false);
-		editInvoiceComponentsJPanel.setLayout(new GridLayout(4,2));
+		editInvoiceComponentsJPanel.setLayout(new GridLayout(5,2));
 		editInvoiceComponentsJPanel.add(new JLabel("Enter New Invoice ID"));
 		editInvoiceComponentsJPanel.add(editInvoiceId);
 		editInvoiceComponentsJPanel.add(new JLabel("Enter New Employee ID"));
 		editInvoiceComponentsJPanel.add(editInvoiceEmployee);
 		editInvoiceComponentsJPanel.add(new JLabel("Enter New Customer ID"));
 		editInvoiceComponentsJPanel.add(editInvoiceCustomer);
-		editInvoiceComponentsJPanel.add(new JLabel("Enter New Product ID"));
-		editInvoiceComponentsJPanel.add(editInvoiceProduct);
-		editInvoiceComponentsJPanel.add(new JLabel("Enter New Quantity"));
-		editInvoiceComponentsJPanel.add(editInvoiceQuantity);
+		editInvoiceComponentsJPanel.add(new JLabel("Total"));
 		editInvoiceComponentsJPanel.add(editInvoiceAmount);
 		editInvoiceComponentsJPanel.add(editPayStatus);
 		editPayStatus.setEditable(false);
@@ -1732,8 +1729,6 @@ public class RetailGUI extends JFrame{
 							editInvoiceId.setText(Integer.toString(id));
 							editInvoiceEmployee.setText(Integer.toString(invoice.getEmployee().getEmployeeId()));
 							editInvoiceCustomer.setText(Integer.toString(invoice.getCustomer().getCustId()));
-							editInvoiceProduct.setText(invoice.getProduct().getProductCode());
-							editInvoiceQuantity.setText(Integer.toString(invoice.getQuantity()));
 							editInvoiceAmount.setText(Double.toString(invoice.getTotalInvoicePrice()));
 							if(invoice.isPaid() == false){
 								editPayStatus.setText("Unpaid");
@@ -1835,8 +1830,6 @@ public class RetailGUI extends JFrame{
 				int invId = 0;						
 				int empId = 0;
 				int custId = 0;
-				String productId = editInvoiceProduct.getText().trim();
-				int qty = 0;
 				int currentId = Integer.parseInt(editInvoiceJTextField.getText().trim());
 				int count1 = 0, count2 = 0, count3 = 0;
 				Employee emp = employees.get(0);
@@ -1846,7 +1839,6 @@ public class RetailGUI extends JFrame{
 					invId = Integer.parseInt(editInvoiceId.getText().trim());
 					empId = Integer.parseInt(editInvoiceEmployee.getText().trim());
 					custId = Integer.parseInt(editInvoiceCustomer.getText().trim());
-					qty = Integer.parseInt(editInvoiceQuantity.getText().trim());
 				}
 				catch(NumberFormatException nfe){
 					invalidId();
@@ -1862,21 +1854,13 @@ public class RetailGUI extends JFrame{
 						cust = customer;
 						count2++;
 					}
-				}						
-				for(Product product: products){
-					if(productId.equals(product.getProductCode())){
-						prod = product;
-						count3++;						
-					}
-				}					
+				}											
 				if (count1 != 0 && count2 != 0 && count3 != 0){
 					for(Invoice invoice: invoices){
 						if(currentId == invoice.getId()){
 							invoice.setId(invId);
 							invoice.setEmployee(emp);
 							invoice.setCustomer(cust);
-							invoice.setProduct(prod);
-							invoice.setQuantity(qty);
 						}
 					}
 					editInvoiceJTextField.setText(Integer.toString(currentId));
@@ -1891,10 +1875,6 @@ public class RetailGUI extends JFrame{
 					JOptionPane.showMessageDialog(null, "Invalid Customer Id!");
 					editInvoiceJButton.doClick();
 				}
-				else if(count3 == 0){
-					JOptionPane.showMessageDialog(null, "Invalid Product Id!");
-					editInvoiceJButton.doClick();
-				}	
 			}
 			public void invalidId(){
 				JOptionPane.showMessageDialog(null, "Id must be a numerical value!");
