faef8414c524c253063630d21e3160bb2246b26d
FCPDIT <ditfcp2014@gmail.com>
2014-08-26 12:58:30 +0100

    Marc: Drop Down combo example

---
+++ RetailSystem/src/retail/ComboBoxExample.java
@@ -0,0 +1,74 @@
+package retail;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class ComboBoxExample extends JFrame{
+	
+	//variables
+	private JPanel panel = new JPanel();
+	private Container container = getContentPane();
+	private JTextField input = new JTextField(10);
+	private JButton button = new JButton("Add");
+	
+	//Components for the Combo Box
+	private Vector<String> listOfLetters  = new Vector<>();
+	private DefaultComboBoxModel<String> list = new DefaultComboBoxModel<>(listOfLetters); //create the combo box
+	private JComboBox<String> comboBox = new JComboBox<String>(list);
+
+	public ComboBoxExample() {
+		//Frame
+		setSize(300, 300);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		//Set ArrayList
+		listOfLetters.add("Happy");
+		listOfLetters.add("Sad");
+		listOfLetters.add("Angry");
+		listOfLetters.add("Tired");
+		
+		//Adding Components
+		panel.add(comboBox);
+		panel.add(input);
+		panel.add(button);
+		container.add(panel);
+		
+		//Dropdown list
+		list.setSelectedItem(listOfLetters.get(0));
+		
+		//button click
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String inputFromTestField = input.getText();
+				list.addElement(inputFromTestField);
+				input.setText("");
+				
+			}
+		});
+		
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ComboBoxExample();
+
+	}
+
+}
