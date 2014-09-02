38f173630d654684f25133ec40f57968b3f422df
marc28 <elmarco18@yahoo.ie>
2014-09-02 14:59:31 +0100

    Marc : Option to have an image button on the Grpah panel

--- RetailSystem/src/retail/RetailGUI.java
+++ RetailSystem/src/retail/RetailGUI.java
@@ -3493,12 +3493,17 @@ public class RetailGUI extends JFrame{
 		graphgc.insets = new Insets(5,5,5,5);
 		//========================== Pie Chart Highest lowest Selling ===============================
 				//TODO high low
-				highestSellingButton = new JButton("Highest selling");
+				highestSellingButton = new JButton("Highest Selling");
 				graphgc.gridx = 1;
 				graphgc.gridy = 1;
 				highestSellingButton.setPreferredSize(d);
 				graphJPanel.add(highestSellingButton,graphgc);
-				
+				/*try{
+					highestSellingButton.setIcon(new ImageIcon("C:\\Users\\marc\\Desktop\\GitHubRepo\\TeamProject\\RetailSystem\\src\\images\\top.png"));
+					
+					}catch(Exception ex){
+						System.out.println("not working");
+					}*/
 				highestSellingButton.addActionListener(new ActionListener() {
 					
 					@SuppressWarnings("rawtypes")
